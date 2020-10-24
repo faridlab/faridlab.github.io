b3d8cccef4a859c9582d89b4e0aae7d12f795f97
Farid Hidayat <e.faridhidayat@gmail.com>
2020-10-24 11:44:40 +0700

    Full name and about

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,44 +2,22 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
+        <!-- <q-btn
           flat
           dense
           round
           icon="menu"
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+        /> -->
 
         <q-toolbar-title>
-          Quasar App
+          Farid Hidayat
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -47,7 +25,7 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+// import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
@@ -92,11 +70,11 @@ const linksData = [
     icon: 'favorite',
     link: 'https://awesome.quasar.dev'
   }
-];
+]
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
+  // components: { EssentialLink },
   data () {
     return {
       leftDrawerOpen: false,
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,43 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="q-pa-md">
+    <div class="q-gutter-md row q-my-sm">
+      <div class="col"><strong class="text-h5">Personal Detail:</strong></div>
+    </div>
+    <div class="q-gutter-md row q-my-sm">
+      <div class="col">
+        <q-input
+          label="Name"
+          value="Farid Hidayat"
+          stack-label
+          filled
+        />
+      </div>
+      <div class="col">
+        <q-input
+          label="Position"
+          value="Senior Fullstack Developer"
+          stack-label
+          filled
+        />
+      </div>
+    </div>
+
+    <div class="q-gutter-md q-my-sm row">
+      <div class="col">
+        <q-editor v-model="about" min-height="5rem" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'Profile',
+  data () {
+    return {
+      about: 'Farid Hidayat has 9 years of experience as an Information Technology professional especially in software development field. Currently Farid is a Product Specialist of PT. Sagara Xinix Solusitama (Sagara). Farid is highly motivated and young visionary to technology enhancement for business and economics. As business technologist, Farid has helped many institutions from government and private sector in Indonesia from initial prototype all the way to production release.'
+    }
+  }
 }
+
 </script>
